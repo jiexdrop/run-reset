40b476aae0ad3f96bddb6ae922b31e42718f2783
jiexdrop <jorge.noogueira@gmail.com>
2026-08-03 10:19:55 +0100

    sapguard drops sword more often

--- autoloads/mob_registry.gd
+++ autoloads/mob_registry.gd
@@ -226,8 +226,9 @@ func _build_registry() -> void:
 	sapguard.attacks     = [sip] as Array[MobAttackData]
 	sapguard.resistances = {"physical": 0.1, "ice": 2.0, "fire": 2.0}
 	sapguard.loot_table  = [
-		{ "item_key": "health_potion", "chance": 0.12, "min": 1, "max": 1 },
-		{ "item_key": "energy_potion", "chance": 0.12, "min": 1, "max": 1 },
+		{ "item_key": "health_potion", "chance": 0.32, "min": 1, "max": 1 },
+		{ "item_key": "energy_potion", "chance": 0.32, "min": 1, "max": 1 },
+		{ "item_key": "iron_sword", "chance": 0.52, "min": 1, "max": 1 },
 	] as Array[Dictionary]
 	_registry["sapguard"] = sapguard
 
